ea70acd003d36821539d85c2bf7805bd021d6828
rasmuskristens <62999973+rasmuskristens@users.noreply.github.com>
2023-02-13 13:12:42 +0100

    startup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,43 @@
-/*=============== FILTERS TABS ===============*/
 
+/*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
+tabs.forEach(tab => tab.addEventListener('click', () => {
+  const target = document.querySelector(tab.dataset.target);
+  tabContents.forEach(tc => {
+    tc.classList.remove('filters__active');
+  });
+  target.classList.add('filters__active');
+  tabs.forEach(t => {
+    t.classList.remove('filter-tab-active');
+  });
+  tab.classList.add('filter-tab-active');
+}));
 
 /*=============== DARK LIGHT THEME ===============*/
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== scroll ===================*/
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } 
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
+
+/*================== toTop ==============*/
+
+//reaches end
+
+window.onscroll = () => {
+  if((window.innerHeight + window.pageYOffset) >=document.body.offsetHeight){
+    console.log("end of page");
+  }
+}
+
+//user to top
+const toTop = () => window.scrollTo({top: 0, behavior:'smooth'})
